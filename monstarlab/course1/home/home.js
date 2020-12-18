26bda02dbadf8c4e00ecab3f212056c3f1eac5d9
LQ <swjtusk@163.com>
2020-12-18 16:23:36 +0800

    12.18

--- monstarlab/course1/home/home.js
+++ monstarlab/course1/home/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // select mkt or tech to display
 let tech = document.getElementsByClassName("tech")[0];
 let mkt = document.getElementsByClassName("mkt")[0];
@@ -24,23 +23,4 @@ var newItems = items.cloneNode(true);
 var box = document.getElementById("contentBox");
 loadMore.onclick = function() {
     box.appendChild(newItems);
-=======
-
-// select mkt or tech to display
-let tech = document.getElementsByClassName("tech")[0];
-let mkt = document.getElementsByClassName("mkt")[0];
-tech.onclick = function () {
-    tech.style.color = "#1c1d1f"
-    mkt.style.color = "#696c6d"
-}
-mkt.onclick = function () {
-    mkt.style.color = "#1c1d1f"
-    tech.style.color = "#696c6d"
-}
-// load more content by Nodeclone
-document.getElementById("loadMore").onclick = function() {
-    let items = document.getElementById("ic");
-    let newItems = items.cloneNode(true);
-    document.getElementById("itemsBox").appendChild(newItems);
->>>>>>> aed1c03b5a4cdd6b341f46c7f387694759397b3b
 }
